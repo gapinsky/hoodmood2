c88d9ca6c7a2f2e13f59d6f4b80f1ec00f411a41
gapinsky <gapinski02@gmail.com>
2026-03-07 10:27:02 +0100

    nav update

--- myComponents/navbar/NavMenuMobile.tsx
+++ myComponents/navbar/NavMenuMobile.tsx
@@ -40,7 +40,12 @@ export default function NavMenuMobile({ isOpen, handleOpenNav }: Props) {
                   <ul>
                     {item.items.map((link) => (
                       <li key={link.label}>
-                        <Link href={link.href}>{link.label}</Link>
+                        <Link
+                          href={link.href}
+                          onClick={() => closeNavOnClick()}
+                        >
+                          {link.label}
+                        </Link>
                       </li>
                     ))}
                   </ul>
@@ -64,9 +69,14 @@ export default function NavMenuMobile({ isOpen, handleOpenNav }: Props) {
             ),
         )}
       </ul>
-      <div className=" flex justify-end gap-2 p-2">
-        <ButtonSecondary />
-        <ButtonPrimary />
+      <div className=" flex justify-end items-center gap-2 p-2 ">
+        <ButtonSecondary href="/kontakt">Kontakt</ButtonSecondary>
+        <Link
+          href="/zapisy"
+          className="whitespace-nowrap uppercase rounded-full  text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-(--brand-700) text-slate-50 hover:bg-(--brand-900) h-9 px-4 py-2 has-[>svg]:px-3 w-fit"
+        >
+          Zapisz się
+        </Link>
       </div>
     </div>
   );
