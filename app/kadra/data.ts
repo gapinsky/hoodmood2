ca521e6dfe8d563e1d4a244c8fbf14b63e4893d2
gapinsky <gapinski02@gmail.com>
2026-08-16 22:14:33 +0200

    tali update, team images folder structure

--- app/kadra/data.ts
+++ app/kadra/data.ts
@@ -22,6 +22,7 @@ export const instructors: Trainer[] = [
     name: "Talita Jarzęcka",
     role: "CEO",
     styles: [
+      "CEO",
       "HYPE CREW (grupa średniozaawansowana hip-hop)",
       "Taniec dla 4–6 latków",
       "Hip-hop ",
@@ -31,7 +32,12 @@ export const instructors: Trainer[] = [
 
     description:
       "Talita to doświadczona choreografka, trenerka i artystka sceniczna, której fundamentem jest solidne wykształcenie muzyczno-taneczne. Ukończyła studia licencjackie na kierunku Taniec Nowoczesny na Wydziale Sztuk Scenicznych Akademii Muzycznej w Łodzi. Wcześniej kształciła się w Szkole Muzycznej im. Grażyny Bacewicz w Koszalinie na Wydziale Rytmiki. Brała udział w spektaklu „Projektowanie człowieka” (chor. Maciej Mołdoch / Sheva), stworzyła autorskie spektakle „Halucynacje” i „Gen Z”. Uczestniczyła w licznych warsztatach i programach artystycznych w Polsce i za granicą (m.in. Ateny, Mediolan, Paryż, Londyn). Od ośmiu lat prowadzi grupę HYPE CREW, która zdobywa miejsca na turniejach. Jej misją jest przygotowywanie młodych tancerzy do profesjonalnego wejścia na scenę.",
-    images: ["/assets/images/team/talitaJarzecka.jpg"],
+    images: [
+      "/assets/images/team/talitaJarzecka/talitaJarzecka.jpg",
+      "/assets/images/team/talitaJarzecka/talitaJarzecka2.jpeg",
+      "/assets/images/team/talitaJarzecka/talitaJarzecka3.jpg",
+
+    ],
     localizations: ["Koszalin", "Polanów", "Biały Bór"],
     instagram: "https://www.instagram.com/talita_jarzecka/",
   },
@@ -48,7 +54,7 @@ export const instructors: Trainer[] = [
 
     description:
       "Wiktoria Butwicka to tancerka i trenerka. Taniec towarzyszy jej od 2009 roku. Szkoliła się m.in. u instruktorów takich jak Klesia, Nikita czy Pumba. Brała udział w licznych turniejach w Polsce i za granicą (m.in. 2. miejsce w Wielkiej Brytanii i Top 8 na Tancbudzie). Korzystała też z prywatnych lekcji i warsztatów w różnych stylach. Jako trenerka uczy od trzech lat – prowadzi grupy hip-hopowe oraz warsztaty dla młodzieży i dorosłych.",
-    images: ["/assets/images/team/wiktoriaButwicka.jpg"],
+    images: ["/assets/images/team/wiktoriaButwicka/wiktoriaButwicka.jpg"],
     localizations: ["Koszalin"],
     instagram:
       "https://www.instagram.com/_._._._wika_._._._?igsh=NWc5Z2w5dW5uNHlu",
@@ -61,7 +67,7 @@ export const instructors: Trainer[] = [
 
     description:
       "Paulina Walikowska rozpoczęła swoją przygodę z tańcem w klasie baletowej, gdzie zdobyła solidne podstawy techniczne. Przez 8 lat rozwijała się artystycznie w zespole HYPE CREW, doskonaląc się jako tancerka sceniczna i instruktorka. Ma doświadczenie warsztatowe u uznanych trenerów, współtworzyła projekty filmowe (w tym produkcję nominowaną na International Cyprus Dance Film Festival) oraz występowała w spektaklach „Halucynacje” i „Gen Z”. Obecnie prowadzi w studiu zajęcia baletu.",
-    images: ["/assets/images/team/paulinaWalikowska.jpg"],
+    images: ["/assets/images/team/paulinaWalikowska/paulinaWalikowska.jpg"],
     localizations: ["Koszalin"],
     instagram:
       "https://www.instagram.com/walikowska?igsh=MWJsazhrcHUxN2k5dA%3D%3D",
@@ -74,7 +80,7 @@ export const instructors: Trainer[] = [
 
     description:
       "Na stronie sekcja Aliny Lemańskiej opisuje start zajęć KPOP — dynamiczne choreografie, energetyczną muzykę i przyjazną atmosferę. Zajęcia są skierowane do osób w różnym wieku i na różnym poziomie zaawansowania, z naciskiem na naukę kroków i choreografii do hitów KPOP oraz dobrą zabawę.",
-    images: ["/assets/images/team/alinaLemanska.jpg"],
+    images: ["/assets/images/team/alinaLemanska/alinaLemanska.jpg"],
     localizations: ["Koszalin"],
     instagram:
       "https://www.instagram.com/alinka_lem?igsh=MXU3bHE5ZmkwMTMxaQ%3D%3D",
@@ -87,7 +93,9 @@ export const instructors: Trainer[] = [
 
     description:
       "Magdalena Sokołowska jest związana z tańcem od 14. roku życia. Jest dyplomowaną instruktorką tańca o specjalizacji hip-hop. Style, w których najlepiej się czuje, to hip-hop i house. Dla siebie tańczy również waacking i high heels. Na co dzień lubi pracę z młodzieżą.",
-    images: ["/assets/images/team/magdalenaSokolowska.jpg"],
+    images: [
+      "/assets/images/team/magdalenaSokolowska/magdalenaSokolowska.jpg",
+    ],
     localizations: ["Koszalin"],
     instagram:
       "https://www.instagram.com/japona_sokolowska?igsh=Yzg3aWM1amVleHB0",
@@ -103,7 +111,7 @@ export const instructors: Trainer[] = [
 
     description:
       "Maria Kober to instruktorka i tancerka z dużym doświadczeniem scenicznym i warsztatowym. Od 8 lat trenuje w ekipie HYPE CREW, z którą wielokrotnie stawała na podium zawodów w Polsce i za granicą. Ma na koncie warsztaty z wieloma instruktorami, treningi w londyńskich studiach Playground i Base oraz udział w projektach wideo (w tym produkcji nominowanej na International Cyprus Dance Film Festival). Prowadzi treningi dla grup THE BEAT HUNTERS i HYPE CREW oraz zajęcia CHOREO.",
-    images: ["/assets/images/team/mariaKober.jpg"],
+    images: ["/assets/images/team/mariaKober/mariaKober.jpg"],
     localizations: ["Koszalin"],
     instagram: "https://www.instagram.com/marysiakober",
   },
@@ -115,7 +123,7 @@ export const instructors: Trainer[] = [
 
     description:
       "Poznajcie naszego trenera,akrobatyka to jego pasja i ogromna część naszych treningów. Aleks ukończył dwuczęściowe szkolenie z zakresu metodyki nauczania oraz techniki wykonywania elementów w skokach na ścieżce. Szczególną uwagę poświęca również bezpieczeństwu, posiada wiedzę i umiejętności z zakresu prawidłowego asekurowania oraz podtrzymywania dzieci podczas nauki nowych elementów, dzięki czemu treningi są nie tylko efektywne, ale przede wszystkim bezpieczne i dostosowane do poziomu każdego uczestnika. Swoje doświadczenie zdobywał, pracując z dziećmi na półkoloniach, w playparku oraz prowadząc regularne zajęcia z akrobatyki dla dzieci i młodzieży.",
-    images: ["/assets/images/team/aleksKultys.jpg"],
+    images: ["/assets/images/team/aleksKultys/aleksKultys.jpg"],
     localizations: ["Koszalin"],
     instagram:
       "https://www.instagram.com/aleks_pk77?igsh=MXU1enAwODQ1ODRzMQ%3D%3D",
@@ -129,8 +137,8 @@ export const instructors: Trainer[] = [
     description:
       "Milena jest absolwentką szkoły muzycznej na wydziale rytmiki, gdzie przez 12 lat zdobywała solidne wykształcenie muzyczno-ruchowe. Doskonałe wyczucie rytmu, świadomość ciała oraz umiejętność łączenia ruchu z muzyką stanowią fundament jej pracy. Dodatkowo gra na fortepianie, co przekłada się na wyjątkową muzykalność i wrażliwość artystyczną. Przez lata rozwijała się w różnych stylach tanecznych, a obecnie specjalizuje się w tańcu współczesnym oraz pole dance. Regularnie uczestniczy w warsztatach tanecznych, nieustannie podnosząc swoje kwalifikacje i poszerzając warsztat instruktorski. Jej doświadczenie, wykształcenie muzyczno-taneczne oraz wszechstronne podejście do pracy z ruchem doskonale wpisują się w kierunek, w którym chcemy się rozwijać. Jesteśmy przekonani, że wniesie do naszych zajęć nową jakość, świeżą energię i profesjonalizm.",
     images: [
-      "/assets/images/team/milenaJasinska.jpg",
-      "/assets/images/team/milenaJasinska2.jpg",
+      "/assets/images/team/milenaJasinska/milenaJasinska.jpg",
+      "/assets/images/team/milenaJasinska/milenaJasinska2.jpg",
     ],
     localizations: ["Koszalin"],
     instagram: "https://www.instagram.com/_m_jasinska?igsh=dDNxamd3N3kyYjN6",
@@ -152,8 +160,8 @@ Taniec to dla niej wolność i ekspresja – jej motto brzmi:
 „Wolność, węże i floorwork!” 🐍
 W 2023 roku została wyróżniona tytułem Trener Roku przez Dziennik Bałtycki, co potwierdza jej ogromne zaangażowanie, pasję i nieustanne sukcesy w pracy z młodymi tancerzami.`,
     images: [
-      "/assets/images/team/kamilaMaik.jpg",
-      "/assets/images/team/kamilaMaik2.jpg",
+      "/assets/images/team/kamilaMaik/kamilaMaik.jpg",
+      "/assets/images/team/kamilaMaik/kamilaMaik2.jpg",
     ],
     localizations: ["Koszalin"],
     instagram: "https://www.instagram.com/kamilamaik",
@@ -166,9 +174,9 @@ W 2023 roku została wyróżniona tytułem Trener Roku przez Dziennik Bałtycki,
 
     description: `Joanna jest instruktorką tańca, trenerką i choreografką w Studio Tańca Joy Dance Szczecin, specjalizującą się w stylach jazz, modern & contemporary, ballet oraz commercial. Jest finalistką, medalistką i stypendystką Global Dance Open oraz finalistką Dance World Cup. Wielokrotnie zdobywała medale i miejsca finałowe Mistrzostw Polski oraz Europy IDO, w tym tytuł Mistrzyni Polski w kategorii show duety oraz II Wicemistrzyni Polski w solo ballet repertoire. Nieustannie rozwija swoje umiejętności pod okiem czołowych choreografów z Polski i świata, a podczas zajęć stawia na technikę, świadomość ciała, muzykalność i rozwój artystyczny każdego tancerza.`,
     images: [
-      "/assets/images/team/joannaJedynak.jpg",
-      "/assets/images/team/joannaJedynak2.jpg",
-      "/assets/images/team/joannaJedynak3.jpg",
+      "/assets/images/team/joannaJedynak/joannaJedynak.jpg",
+      "/assets/images/team/joannaJedynak/joannaJedynak2.jpg",
+      "/assets/images/team/joannaJedynak/joannaJedynak3.jpg",
     ],
     localizations: ["Koszalin"],
     instagram: "https://www.instagram.com/joanna.jedynak_",
@@ -187,7 +195,7 @@ Na swoim koncie ma liczne sukcesy na zawodach tanecznych. W tym sezonie zwycię�
 Jej doświadczenie wykracza poza scenę zawodów,obejmuje projekty komercyjne, produkcje reklamowe, teledyski, choreografię, pracę zespołową oraz sędziowanie zawodów tanecznych. Dzięki temu swobodnie porusza się zarówno w świecie rywalizacji tanecznej, jak i profesjonalnych realizacji scenicznych, medialnych i komercyjnych.
 
 W tańcu łączy sportową determinację, wyjątkową muzykalność, mocny charakter i indywidualny styl. Jako trenerka i instruktorka przekazuje swoje doświadczenie kolejnym pokoleniom tancerzy, jednocześnie nieustannie rozwijając własny język ruchu i artystyczną tożsamość.`,
-    images: ["/assets/images/team/nikolaSuchocka.jpg"],
+    images: ["/assets/images/team/nikolaSuchocka/nikolaSuchocka.jpg"],
     localizations: ["Koszalin"],
     instagram: "https://www.instagram.com/nikolasuchocka",
   },
